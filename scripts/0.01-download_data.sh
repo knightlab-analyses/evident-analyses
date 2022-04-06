f68c04232486c93b2942f8982724574d0556d894
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-04-06 14:24:48 -0700

    Update download script

--- scripts/0.01-download_data.sh
+++ scripts/0.01-download_data.sh
@@ -18,6 +18,7 @@ CTX="Deblur_2021.09-Illumina-16S-V4-100nt-50b3a2"
 SAMPLE_FILE="data/raw/samples.txt"
 TABLE="data/raw/table.raw.biom"
 MD="data/raw/metadata.raw.tsv"
+MD_PREP="data/raw/metadata.prep.raw.tsv"
 DM="data/ref/unweighted.txt.gz"
 
 python -c "from skbio import DistanceMatrix; dm = DistanceMatrix.read('${DM}'); x = dm.ids; f = open('${SAMPLE_FILE}', 'w'); f.writelines('\n'.join(x)); f.close()"
@@ -33,9 +34,17 @@ echo "[ $(date) ] :: Start redbiom fetch sample metadata"
 redbiom fetch sample-metadata \
     --from $SAMPLE_FILE \
     --context $CTX \
-    --tagged \
+    --resolve-ambiguities \
     --all-columns \
     --output $MD
+
+echo "[ $(date) ] :: Start redbiom fetch sample prep metadata"
+redbiom fetch sample-metadata \
+    --from $SAMPLE_FILE \
+    --context $CTX \
+    --tagged \
+    --all-columns \
+    --output $MD_PREP
 
 echo "[ $(date) ] :: Finished script!"
 
