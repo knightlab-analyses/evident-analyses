1776ac41f674f7426479cfb3a54a2d7fa7caeecf
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-04-07 08:05:28 -0700

    Add sepp script

---
+++ scripts/2.01-sepp.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+#SBATCH --chdir=/home/grahman/projects/evident-analyses
+#SBATCH --output=/home/grahman/projects/evident-analyses/log/%x.log
+#SBATCH --partition=long
+#SBATCH --nodes=4
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=16G
+#SBATCH --time=24:00:00
+
+echo "[ $(date) ] :: Start script"
+date; pwd; hostname
+
+source ~/miniconda3/bin/activate evident-analyses
+
+TBL="data/processed/table.rare.filt.biom"
+FNA=$(realpath "data/processed/seqs.fna")
+python -c "import biom; t = biom.load_table('$TBL'); f = open('$FNA', 'w'); f.write(''.join(['>%s\n%s\n' % (i, i.upper()) for i in t.ids(axis='observation')]))"
+
+SEPP="/home/grahman/software/sepp-package/run-sepp.sh"
+
+OUTDIR="results/processed/sepp"
+mkdir -p $OUTDIR
+cd $OUTDIR
+
+echo "[ $(date) ] :: Running SEPP..."
+
+$SEPP $FNA tbl_gg -x 32
+
+echo "[ $(date) ] :: Finished script!"
